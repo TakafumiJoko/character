02a9167132798cefec02ff7df8b2a6c5d8252c35
Takafumi <jktkfm5535@gmail.com>
2020-09-21 12:59:35 +0000

    guest-login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,4 +19,13 @@ class SessionsController < ApplicationController
     flash[:success] = "ログアウトに成功しました。"
     redirect_to login_url 
   end
+  
+  def create_guest
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.name = 'guest'
+      user.password = SecureRandom.urlsafe_base64
+    end
+    log_in user
+    redirect_to new_post_url, notice: 'ゲストユーザーとしてログインしました。'
+  end
 end
